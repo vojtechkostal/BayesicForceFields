345eaf52ccb49459a5c02a046bd9c0442fd21091
kostalv <vojtakostal8@gmail.com>
2025-06-17 15:56:39 +0200

    updated logic

--- bff/workflow/simulate.py
+++ bff/workflow/simulate.py
@@ -1,28 +1,30 @@
 import argparse
 import os
 import subprocess
+import shutil
 
 import numpy as np
 
 from pathlib import Path
 
-from amo.io.mdp import get_n_frames_target
-from amo.structures import Specs
-from amo.topology import TopologyParser
-from amo.io.utils import load_yaml
+from ..io.mdp import get_n_frames_target
+from ..structures import Specs
+from ..topology import TopologyParser
+from ..io.utils import load_yaml
 
 
-def check_success(fn_trj, fn_mdp, n_target):
+def check_success(fn_trj: str, fn_mdp: str, n_target: int) -> bool:
     """Check if the trajectory has reached the target number of frames."""
     output = subprocess.run(
         ['gmx', 'check', '-f', fn_trj], capture_output=True, text=True)
-    step_line = [line.split()[1]
-                 for line in output.stderr.splitlines()
-                 if line.startswith('Step')]
-    n = int(step_line[0])
+    
+    for line in output.stderr.splitlines():
+        if line.startswith("Step"):
+            n = int(line.split()[1])
+            break
 
     n_target_mdp, stride = get_n_frames_target(fn_mdp)
-    n_target = (n_target // stride) if n_target != -2 else n_target_mdp
+    n_target = n_target_mdp if n_target == -2 else (n_target // stride)
 
     return n >= n_target
 
@@ -54,57 +56,58 @@ def modify_topology(
     return topol
 
 
-def main(fn_config):
+def main(fn_config: str) -> None:
 
-    # Parse the command-line arguments and load specs
+    # Parse the command-line arguments load general configuration
     config = load_yaml(fn_config)
     hash = config['hash']
     params = config['params']
-    if config.get('fn_specs'):
-        specs = Specs(config['fn_specs'])
-    else:
-        specs = Specs(config)
-
-    # Run Gromacs simulation(s)
-    success = []
-    implicit = config.get('implicit', True)
     data_dir = Path(config['data_dir'])
+    specs = Specs(config['fn_specs']) if config.get('fn_specs') else Specs(config)
+    implicit = config.get('implicit', True)
+    gmx_cmd = config.get('gmx_cmd', 'gmx')
+
+    # Unpack Gromacs configuration
+    job_keys = ['fn_mdp_em', 'fn_mdp_prod', 'fn_coordinates', 'fn_ndx', 'n_steps']
+    fn_topol = config['gromacs']['fn_topol']
+    for fn_group in job_keys:
+        if isinstance(config['gromacs'][fn_group], str):
+            config['gromacs'][fn_group] = [config['gromacs'][fn_group]] * len(fn_topol)
+            
     fn_mdp_em = config['gromacs']['fn_mdp_em']
     fn_mdp_prod = config['gromacs']['fn_mdp_prod']
     fn_coord = config['gromacs']['fn_coordinates']
-    fn_topol = config['gromacs']['fn_topol']
     fn_ndx = config['gromacs']['fn_ndx']
-    n_jobs = len(fn_mdp_prod)
-    n_steps = config['gromacs'].get('n_steps', -2)
-    n_steps = np.repeat(n_steps, n_jobs) if isinstance(
-        n_steps, int) else n_steps
+    n_steps = config['gromacs']['n_steps']
+
+    # Determine the run directory
+    job_scheduler = config.get('job_scheduler', 'local')
+    run_dir = data_dir if job_scheduler == 'local' else Path('./').resolve()
+
+    # Define the output file names
+    deffnm = run_dir / f"md-{hash}-{i}"
+    fn_tpr = run_dir / f"{deffnm}.tpr"
+
     fn_log = str(data_dir / 'gmx.log')
+    md_specs = [fn_mdp_em, fn_mdp_prod, fn_coord, fn_topol, fn_ndx, n_steps]
+    success = []
     with open(fn_log, 'a+') as log:
-        for i, (em, prod, c, t, ndx, n) in enumerate(
-                zip(fn_mdp_em, fn_mdp_prod, fn_coord, fn_topol, fn_ndx, n_steps)):
+        for i, (em, prod, c, t, ndx, n) in enumerate(zip(md_specs)):
 
-            run_dir = (
-                Path('./').resolve()
-                if config['execution_mode'] in ['slurm']
-                else data_dir
-            )
-            deffnm = run_dir / f"md-{hash}-{i}"
-            fn_tpr = run_dir / f"{deffnm}.tpr"
-
-            # Create the topology file
+            # Create topology with new parameters
             fn_top_new = str(run_dir / f"topol-{hash}-{i:03d}.top")
             _ = modify_topology(t, specs, params, implicit, fn_top_new)
 
             # Minimize energy
             if em:
                 subprocess.run(
-                    ['gmx', 'grompp', '-f', em, '-c', c, '-p',
+                    [gmx_cmd, 'grompp', '-f', em, '-c', c, '-p',
                      fn_top_new, '-n', ndx, '-o', fn_tpr, '-maxwarn', '2'],
                     cwd=run_dir, stdout=log, stderr=log
                 )
 
                 subprocess.run(
-                    ['gmx', 'mdrun', '-s', fn_tpr, '-deffnm', deffnm],
+                    [gmx_cmd, 'mdrun', '-s', fn_tpr, '-deffnm', deffnm],
                     cwd=run_dir, stdout=log, stderr=log
                 )
             else:
@@ -112,13 +115,13 @@ def main(fn_config):
 
             # Run production MD
             subprocess.run(
-                ['gmx', 'grompp', '-f', prod, '-c', deffnm.with_suffix('.gro'),
+                [gmx_cmd, 'grompp', '-f', prod, '-c', deffnm.with_suffix('.gro'),
                  '-p', fn_top_new, '-n', ndx, '-o', fn_tpr, '-maxwarn', '2'],
                 cwd=run_dir, stdout=log, stderr=log
             )
 
             subprocess.run(
-                ['gmx', 'mdrun', '-s', fn_tpr, '-deffnm', deffnm,
+                [gmx_cmd, 'mdrun', '-s', fn_tpr, '-deffnm', deffnm,
                  '-nsteps', str(n), '-dlb', 'yes', '-ntmpi', '1'],
                 cwd=run_dir, stdout=log, stderr=log
             )
@@ -127,14 +130,16 @@ def main(fn_config):
             success.append(check_success(f'{deffnm}.xtc', prod, n))
 
     if np.all(success):
-        if config['execution_mode'] == 'local':
-            pass
-        else:
-            os.system(f"cp *.xtc {data_dir}")
-            if config.get('store', False):
-                os.system(f"cp *.log *.edr *.tpr *.top {data_dir}")
+        if job_scheduler != 'local':
+            pattern = "*" if config.get('store', False) else "*.xtc"
+            for file in run_dir.glob(pattern):
+                shutil.copy(file, data_dir)
     else:
-        os.system(f"rm {deffnm}-*.xtc")
+        for file in run_dir.glob(f"*.xtc"):
+            try:
+                Path(file).unlink()
+            except FileNotFoundError:
+                pass
 
 
 if __name__ == "__main__":
