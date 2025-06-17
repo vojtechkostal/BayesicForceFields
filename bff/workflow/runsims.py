d18f115492962e6846027f730e1fd7eaf068c8f2
kostalv <vojtakostal8@gmail.com>
2025-06-17 13:39:51 +0200

    simplified logic and generality

--- bff/workflow/runsims.py
+++ bff/workflow/runsims.py
@@ -7,56 +7,72 @@ from pathlib import Path
 from ..topology import TopologyParser
 from ..structures import Specs, RandomParamsGenerator
 from ..io.utils import load_yaml, save_yaml, compress_results
-from ..io.slurm import Slurm
+from ..io.schedulers import Slurm
 
 
-def dispatch_md_job(hash, sample, config):
-    """
-    Submit an MD simulation job using Slurm.
-    """
+SCHEDULER_CLASSES = {
+    'slurm': Slurm,
+    # future: 'pbs': PBS,
+}
 
-    config_md = config | {'params': sample, 'hash': hash}
-    data_dir = Path(config_md['data_dir']).resolve()
-    FN_MD_SCRIPT = 'BayesicForceFields.bff.workflow.simulate'
+FN_MD_SCRIPT = Path(__file__).resolve().parent / 'simulate.py'
 
-    # Change to full paths
-    if config_md.get('fn_specs'):
-        config_md['fn_specs'] = str(Path(config_md['fn_specs']).resolve())
-    config_md['data_dir'] = str(Path(config_md['data_dir']).resolve())
-    for fn_group, items in config_md['gromacs'].items():
-        if fn_group != 'n_steps':
-            config_md['gromacs'][fn_group] = [
+
+def resolve_config_paths(config: dict) -> dict:
+    # Resolve key paths
+    for key in ['fn_specs', 'data_dir']:
+        if key in config:
+            config[key] = str(Path(config[key]).resolve())
+    
+    # Resolve GROMACS file paths
+    for fn_group, items in config['gromacs'].items():
+        if fn_group == 'n_steps':
+            config['gromacs'][fn_group] = items
+        else:
+            config['gromacs'][fn_group] = [
                 str(Path(f).resolve()) for f in items
             ]
-        else:
-            config_md['gromacs'][fn_group] = items
 
-    # Save the simulation-specific config file
+    return config
+
+
+def dispatch_md_job(hash, sample, config, job_scheduler):
+    """
+    Submit an MD simulation either locally or via a job scheduler.
+    """
+
+    # Prepare job-specific configurations
+    config_md = resolve_config_paths(config) | {'params': sample, 'hash': hash}
+    data_dir = Path(config_md['data_dir'])
     fn_config_md = data_dir / f'config-{hash}.yaml'
     save_yaml(config_md, fn_config_md)
 
-    cmd_list = [
-        config['python'],
-        '-m',
-        FN_MD_SCRIPT,
-        '-f',
-        fn_config_md
-    ]
+    cmd_run = [config['python'], str(FN_MD_SCRIPT), '-f', str(fn_config_md)]
 
-    if config['execution_mode'] == 'slurm':
-        fn_submit = data_dir / f'run-{hash}.sh'
-        fn_slurm = data_dir / f'slurm-{hash}.out'
+    if job_scheduler == 'local':
+        subprocess.run(cmd_run, cwd=str(data_dir))
+        return None
 
-        slurm_script = Slurm(**config['slurm']['sbatch'] | {'output': fn_slurm})
-        for cmd in config['slurm']['commands']:
-            if 'RUN MD' in cmd:
-                cmd = ' '.join(map(str, cmd_list))
-            slurm_script.add_command(cmd)
-        id = slurm_script.submit(fn_submit)
-        return id
+    submit_cls = SCHEDULER_CLASSES[job_scheduler]
+    submit_script = submit_cls(**submit_specs)
 
-    elif config.get('execution_mode') == 'local':
-        subprocess.run(cmd_list, cwd=str(data_dir))
+    fn_submit = data_dir / f'run-{hash}.sh'
+    fn_stdout = data_dir / f'run-{hash}.out'
+
+    if config.get(job_scheduler) is None:
+        raise ValueError(
+            f"Configuration for job scheduler '{job_scheduler}' is missing."
+        )
+    
+    submit_cls = SCHEDULER_CLASSES[job_scheduler]
+    submit_specs = config[job_scheduler]['preamble'] | {'output': fn_stdout}
+    submit_script = submit_cls(**submit_specs)
+
+    for cmd in config[job_scheduler]['commands']:
+        resolved = ' '.join(map(str, cmd_run)) if 'RUN MD' in cmd else cmd
+        submit_script.add_command(resolved)
+
+    return submit_script.submit(fn_submit)
 
 
 # ---- Initialization ----
@@ -65,11 +81,13 @@ def initialize_environment(config):
     Set up the directory structure and save settings and initial data.
     """
 
+    # Create the data directory
     data_dir = Path(config['data_dir']).resolve()
     data_dir.mkdir(parents=True, exist_ok=True)
 
+    # Check if Specs file is provided or needs to be generated
     if not config.get('fn_specs'):
-        topol = TopologyParser(config['gromacs']['fn_topol'][0])
+        topol = TopologyParser(config['gromacs']['fn_topol'][0]) # NOTE: assumes the mol_resname is the same for all topologies
         topol.select_mol(config['mol_resname'], config['implicit_atomtype'])
         specs_data = {
             'mol_resname': config['mol_resname'],
@@ -79,51 +97,57 @@ def initialize_environment(config):
             'total_charge': config['total_charge'],
         }
 
+        # Save the Specs data into a file
         fn_specs = data_dir / 'specs.yaml'
         Specs(specs_data).save(fn_specs)
     else:
         fn_specs = Path(config['fn_specs']).resolve()
 
-    for i, (fn_top, fn_coords, fn_mdp, fn_ndx) in enumerate(
-        zip(
-            config['gromacs']['fn_topol'],
-            config['gromacs']['fn_coordinates'],
-            config['gromacs']['fn_mdp_prod'],
-            config['gromacs']['fn_ndx']
-        )
-    ):
-        top = TopologyParser(fn_top)
-        fn_top_out = data_dir / f'topol-{i:03d}.top'
-        top.write(str(fn_top_out))
+    gmx_keys = ['fn_topol', 'fn_coordinates', 'fn_mdp_em', 'fn_mdp_prod', 'fn_ndx']
+    files = zip(*(config['gromacs'][key] for key in gmx_keys))
+    for i, (fn_top, fn_coords, fn_mdp_em, fn_mdp_prod, fn_ndx) in enumerate(files):
 
-        fn_coords_out = data_dir / f'coords-{i:03d}.gro'
-        fn_coords_out.write_text(Path(fn_coords).read_text())
+        # Write topology file
+        TopologyParser(fn_top).write(str(data_dir / f'topol-{i:03d}.top'))
 
-        fn_mdp_out = data_dir / f'nvt-{i:03d}.mdp'
-        fn_mdp_out.write_text(Path(fn_mdp).read_text())
+        # File mapping: (source, destination filename template)
+        file_map = {
+            fn_coords: f'coords-{i:03d}.gro',
+            fn_mdp_em: f'md-em-{i:03d}.mdp',
+            fn_mdp_prod: f'md-prod-{i:03d}.mdp',
+            fn_ndx: f'index-{i:03d}.ndx',
+        }
 
-        fn_ndx_out = data_dir / f'index-{i:03d}.ndx'
-        fn_ndx_out.write_text(Path(fn_ndx).read_text())
+        # Copy files to the data directory
+        for src, dst_name in file_map.items():
+            dst_path = data_dir / dst_name
+            dst_path.write_text(Path(src).read_text())
 
     return fn_specs
 
 
 # ---- Job Monitoring ----
-def get_active_jobs(ids):
+def get_active_jobs(ids, scheduler):
     """Get the number of active jobs from a list of job IDs."""
     ids_str = ','.join(ids)
-    jobs = subprocess.run(
-        ['squeue', '-j', ids_str, '--noheader', '--format', '%i,%t'],
-        capture_output=True,
-        text=True)
+
+    if scheduler == 'slurm':
+        jobs = subprocess.run(
+            ['squeue', '-j', ids_str, '--noheader', '--format', '%i,%t'],
+            capture_output=True,
+            text=True)
+    else:
+        raise NotImplementedError(
+            f"Job scheduler '{scheduler}' is not supported for job monitoring."
+        )
     n_active = jobs.stdout.count('\n')
     return n_active
 
 
-def control_jobs(job_ids):
+def control_jobs(job_ids, scheduler):
     """Monitor active jobs until completion."""
     while True:
-        if get_active_jobs(job_ids) == 0:
+        if get_active_jobs(job_ids, scheduler) == 0:
             break
         time.sleep(5)
 
@@ -134,6 +158,7 @@ def clean_up_train_dir(samples, data_dir, compress=False, remove=False):
     Remove failed simulations and optionally compress results.
     """
 
+    data_dir = Path(data_dir)
     samples_to_store = {}
     for hash, params in samples.items():
         fn_trial = sorted(data_dir.glob(f'*{hash}*.xtc'))  # sort by filename
@@ -143,7 +168,7 @@ def clean_up_train_dir(samples, data_dir, compress=False, remove=False):
                 'fn_trj': [f.name for f in fn_trial]
             }
 
-    save_yaml(samples_to_store, os.path.join(data_dir, 'samples.yaml'))
+    save_yaml(samples_to_store, data_dir / 'samples.yaml')
 
     if compress:
         compress_results(data_dir)
@@ -158,13 +183,11 @@ def print_train_summary(config):
     Print a summary of the configuration settings.
     """
     print()
-    print(' Generating training set '.center(40, '-'))
-    print()
-    print(f"> molecule: {config['mol_resname']}")
-    print("> parameters:")
+    print(f'> Generating training set for: {config['mol_resname']}')
+    print("  > parameters:")
     for name, b in config['bounds'].items():
         print(f"    {name}: {b}")
-    print(f"> total charge: {config['total_charge']}")
+    print(f" > total charge: {config['total_charge']}")
     print()
 
 
@@ -173,12 +196,12 @@ def print_validate_summary(config):
     Print a summary of the configuration settings.
     """
     print()
-    print(' Generating validation set '.center(40, '-'))
+    print(f'> Generating validation set for: {config['mol_resname']}')
     print()
 
 
 # ---- Main Workflow ----
-def main(mode, fn_config):
+def main(fn_config):
     """
     Main function to execute the training set generation.
     """
@@ -186,90 +209,86 @@ def main(mode, fn_config):
     # Initialization
     config = load_yaml(fn_config)
     fn_specs = initialize_environment(config)
-    execution_mode = config.get('execution_mode', 'local')
+    validate = 'inputs' in config
 
-    if mode == 'train':
-        print_train_summary(config)
-        sampler = RandomParamsGenerator(fn_specs)
-        iterator = range(config['n_samples'])
-        n_total_tasks = config['n_samples']
-    elif mode == 'validate':
+    if validate:
         print_validate_summary(config)
         inputs = np.load(config['inputs'])
         iterator = inputs
-        n_total_tasks = len(inputs)
+        n_total = len(inputs)
     else:
-        raise ValueError(
-            f"Unknown mode: {mode}. The mode must be either 'train' or 'validate'."
-        )
+        print_train_summary(config)
+        sampler = RandomParamsGenerator(fn_specs)
+        iterator = range(config['n_samples'])
+        n_total = config['n_samples']
 
     # Main loop to generate samples
     samples = {}
     job_ids = []
     for idx, p in enumerate(iterator):
 
+        print(
+            f"> Running MD: {idx+1}/{n_total} "
+            f"({((idx + 1) / n_total * 100):.0f}%)\r",
+            end='', flush=True
+        )
+
         # Generate sample or use provided input
-        if mode == 'train':
-            max_attempts = 100
-            for attempt in range(max_attempts):
+        if validate:
+            hash, sample = str(idx), p
+        else:
+            max_attempts = 1000
+            for _ in range(max_attempts):
                 hash, sample = sampler.generate(1, assign_hash=True)
                 if sample.size > 0:
                     sample = sample.squeeze(0)
                     break
-            if attempt == max_attempts:
+            else:
                 raise RuntimeError(
-                    "Failed to generate a valid sample after 100 attempts."
-                )
-        elif mode == 'validate':
-            hash, sample = str(idx), p
+                    f"Failed to generate valid sample after {max_attempts} attempts.")
 
         # Store sample info
         samples[hash] = sample
 
         # Submit job while controling total number of running jobs
-        if execution_mode == 'slurm':
-            n_max = config['slurm'].get('max_parallel_jobs', 1)
+        job_scheduler = config.get('job_scheduler', 'local')
+        if job_scheduler == 'local':
+            dispatch_md_job(hash, sample, config, job_scheduler)
+
+        elif job_scheduler not in SCHEDULER_CLASSES:
+            raise NotImplementedError(
+                f"Unsupported scheduler '{job_scheduler}'. "
+                f"Supported: {list(SCHEDULER_CLASSES)}"
+            )
+        
+        else:
+            n_max = config[job_scheduler].get('max_parallel_jobs', 1)
             n_max = np.inf if n_max == -1 else n_max
             while True:
                 if n_max > 0:
-                    n_active = get_active_jobs(job_ids)
-                    if n_active < n_max and idx < n_total_tasks:
-                        id = dispatch_md_job(hash, sample, config)
-                        job_ids.append(id)
+                    n_active = get_active_jobs(job_ids, job_scheduler)
+                    if n_active < n_max and idx < n_total:
+                        id = dispatch_md_job(hash, sample, config, job_scheduler)
+                        if id is not None:
+                            job_ids.append(id)
                         break
                     else:
                         time.sleep(5)
                 else:
                     break
 
-        elif execution_mode == 'local':
-            dispatch_md_job(hash, sample, fn_config)
+    # Wait for jobs to finish if using a job scheduler
+    if job_scheduler != 'local' and n_max > 0:
+        control_jobs(job_ids, job_scheduler)
 
-        else:
-            raise NotImplementedError(
-                f"Execution manager '{execution_mode}' is not supported."
-                "Please use 'slurm' or 'local'."
-            )
-
-        print(
-            f"> Running MD: {idx+1}/{n_total_tasks} "
-            f"({((idx + 1) / n_total_tasks * 100):.0f}%)\r",
-            end='', flush=True
-        )
-
-    # Wait for jobs to finish if using Slurm
-    if execution_mode == 'slurm':
-        if n_max > 0:
-            control_jobs(job_ids)
-
-    print(f'> Running MD: {n_total_tasks}/{n_total_tasks} (100%) | Done.'.ljust(50))
+    print(f'> Running MD: {n_total}/{n_total} (100%) | Done.'.ljust(50))
 
     # Cleanup
     clean_up_train_dir(
         samples,
         Path(config['data_dir']),
-        compress=config['compress'],
-        remove=config['cleanup']
+        compress=config.get('compress', False),
+        remove=config.get('remove', False)
     )
 
 
@@ -279,7 +298,6 @@ if __name__ == "__main__":
             'Generate training/validation set for MD simulations.'
         )
     )
-    parser.add_argument('mode', help='Mode to run the workflow [train|validate].')
     parser.add_argument('fn_config', help='Path to the config file [YAML].')
     args = parser.parse_args()
-    main(args.mode, args.fn_config)
+    main(args.fn_config)
