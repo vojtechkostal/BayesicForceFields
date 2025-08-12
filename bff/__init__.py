d430e49d48540292a14e4e46ac3ebdda1d5264b3
kostalv <vojtakostal8@gmail.com>
2025-08-12 16:13:42 +0200

    qoi renaming

--- bff/__init__.py
+++ bff/__init__.py
@@ -1,6 +1,6 @@
 from .bff import Optimizer
 from . import postprocessing
-from . import evaluation
+from . import qoi
 from . import data
 from . import topology
 from . import tools
@@ -13,7 +13,7 @@ __version__ = "0.1.0"
 __all__ = [
     "Optimizer",
     "postprocessing",
-    "evaluation",
+    "qoi",
     "data",
     "topology",
     "tools",
--- bff/qoi/__init__.py
+++ bff/qoi/__init__.py
@@ -2,12 +2,10 @@ from . import hbonds
 from . import restraints
 from . import rdf
 from . import trajectory
-from . import metrics
 
 __all__ = [
     "hbonds",
     "restraints",
     "rdf",
     "trajectory",
-    "metrics"
 ]
