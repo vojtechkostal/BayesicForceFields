fixed constraint matrix calculation

bff/structures.py
@@ -385,14 +385,13 @@ class Specs:
         coeffs: list[int] = []
 
         for param in self.bounds.names:
-            if not param.startswith("charge"):
-                continue
-
             atoms = param.split()[1:]
-            if atoms == self.implicit_atoms:
-                continue
-
-            coeffs.append(len(atoms))
+            if param.startswith("charge"):
+                if atoms == self.implicit_atoms:
+                    continue
+                coeffs.append(len(atoms))
+            else:
+                coeffs.append(0)
 
         return np.asarray(coeffs, dtype=int)
 
@@ -422,7 +421,7 @@ class ChargeConstraint(Specs):
 
         self.explicit_bounds = self.bounds.without(self.implicit_param).array
         self.implicit_bounds = self.bounds.by_name[self.implicit_param]
-
+   
         if self.explicit_bounds.ndim != 2 or self.explicit_bounds.shape[1] != 2:
             raise ValueError("Bounds must be a 2D array with shape (n_params, 2).")
 