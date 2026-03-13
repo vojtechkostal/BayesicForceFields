62a86d65b63100f19fca9f84b69408c666b736ec
kostalv <vojtakostal8@gmail.com>
2026-03-13 15:45:00 +0100

    rdf_sigmoid with explicit parameters

--- bff/tools.py
+++ bff/tools.py
@@ -77,10 +77,9 @@ def sigmoid(x: np.ndarray, x0: float = 3, scale: float = 5) -> np.ndarray:
     return 1 / (1 + np.exp(- arg))
 
 
-def rdf_sigmoid_mean(settings: dict, ref_rdf: np.ndarray) -> np.ndarray:
+def rdf_sigmoid_mean(n_bins: int, r_range: tuple, ref_rdf: np.ndarray) -> np.ndarray:
     """Create a sigmoid function for concatenated RDFs."""
-    n_bins = settings['rdf_kwargs']['n_bins']
-    r0, r1 = settings['rdf_kwargs']['r_range']
+    r0, r1 = r_range
     dr_half = (r1 - r0) / (2 * n_bins)
     r = np.linspace(r0, r1, n_bins, endpoint=False) + dr_half
     n_rdf = ref_rdf.size // n_bins
